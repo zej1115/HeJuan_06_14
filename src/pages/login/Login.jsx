6d3d4d68ad9e53af0ab2399a3d85fe16f3bf88fe
hejuan <hejuan@sina.com>
2019-06-14 21:09:32 +0800

    02msg

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import logo from './image/logo.png'
+import { Form, Icon, Input, Button} from 'antd';
 import "./login.less"
+const Item = Form.Item
 export default class Login extends Component{
     render(){
         return(
@@ -11,7 +13,26 @@ export default class Login extends Component{
                 </header>
                 <section className = "login_content">
                     <h2>用户登陆</h2>
-                    <div>hjh</div>
+                    <Form onSubmit={this.handleSubmit} className="login-form">
+                        <Item>
+                        <Input
+                            prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                            placeholder="用户名"
+                        />
+                        </Item>
+                        <Form.Item>
+                        <Input
+                            prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                            type="password"
+                            placeholder="密码"
+                        />
+                        </Form.Item>
+                        <Form.Item>
+                        <Button type="primary" htmlType="submit" className="login-form-button">
+                            登 陆
+                        </Button>
+                        </Form.Item>
+                    </Form>
                 </section>
               
             </div>
